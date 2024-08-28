8e4ed4b880cbcffbb037a60722499ed9651a45d8
jpsteph <42590169+jpsteph@users.noreply.github.com>
2024-08-28 19:06:27 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -55,6 +55,58 @@ DMA_HandleTypeDef hdma_usart2_tx;
 /* USER CODE BEGIN PV */
 
 
+uint8_t BMW_KEYFOB[] = { //In fact it's 2.4Kb/s because of the Manhattan codification, see Datasheet.
+	    0x0D,  // IOCFG2              GDO2 Output Pin Configuration SERIAL DATA OUT
+	    0x2E,  // IOCFG1              GDO1 Output Pin Configuration
+	    0x08,  // IOCFG0              GDO0 Output Pin Configuration
+	    0x47,  // FIFOTHR             RX FIFO and TX FIFO Thresholds
+	    0xD3,  // SYNC1               Sync Word, High Byte
+	    0x91,  // SYNC0               Sync Word, Low Byte
+	    0xFF,  // PKTLEN              Packet Length
+	    0x00,  // PKTCTRL1            Packet Automation Control
+	    0x32,  // PKTCTRL0            Packet Automation Control, IS IN ASYCH MODE
+	    0x00,  // ADDR                Device Address
+	    0x00,  // CHANNR              Channel Number
+	    0x06,  // FSCTRL1             Frequency Synthesizer Control
+	    0x00,  // FSCTRL0             Frequency Synthesizer Control
+	    0x21,  // FREQ2               Frequency Control Word, High Byte
+	    0x62,  // FREQ1               Frequency Control Word, Middle Byte
+	    0x76,  // FREQ0               Frequency Control Word, Low Byte
+	    0xF8,  // MDMCFG4             Modem Configuration
+	    0xA0,  // MDMCFG3             Modem Configuration //settings should be 3.4kb
+	    0x33,  // MDMCFG2             Modem Configuration //need to change to 0 or x4
+	    0x22,  // MDMCFG1             Modem Configuration
+	    0xF8,  // MDMCFG0             Modem Configuration
+	    0x15,  // DEVIATN             Modem Deviation Setting
+	    0x07,  // MCSM2               Main Radio Control State Machine Configuration
+	    0x30,  // MCSM1               Main Radio Control State Machine Configuration
+	    0x18,  // MCSM0               Main Radio Control State Machine Configuration
+	    0x16,  // FOCCFG              Frequency Offset Compensation Configuration
+	    0x6C,  // BSCFG               Bit Synchronization Configuration
+	    0x04,  // AGCCTRL2            AGC Control
+	    0x00,  // AGCCTRL1            AGC Control
+	    0x92,  // AGCCTRL0            AGC Control
+	    0x87,  // WOREVT1             High Byte Event0 Timeout
+	    0x6B,  // WOREVT0             Low Byte Event0 Timeout
+	    0xFB,  // WORCTRL             Wake On Radio Control
+	    0x56,  // FREND1              Front End RX Configuration
+	    0x11,  // FREND0              Front End TX Configuration
+	    0xE9,  // FSCAL3              Frequency Synthesizer Calibration
+	    0x2A,  // FSCAL2              Frequency Synthesizer Calibration
+	    0x00,  // FSCAL1              Frequency Synthesizer Calibration
+	    0x1F,  // FSCAL0              Frequency Synthesizer Calibration
+	    0x41,  // RCCTRL1             RC Oscillator Configuration
+	    0x00,  // RCCTRL0             RC Oscillator Configuration
+	    0x59,  // FSTEST              Frequency Synthesizer Calibration Control
+	    0x7F,  // PTEST               Production Test
+	    0x3F,  // AGCTEST             AGC Test
+	    0x81,  // TEST2               Various Test Settings
+	    0x35,  // TEST1               Various Test Settings
+	    0x09,  // TEST0               Various Test Settings
+
+};
+
+
 uint8_t JETTA_KEYFOB[] = { //In fact it's 2.4Kb/s because of the Manhattan codification, see Datasheet.
 	    0x0D,  // IOCFG2              GDO2 Output Pin Configuration SERIAL DATA OUT
 	    0x2E,  // IOCFG1              GDO1 Output Pin Configuration
@@ -257,6 +309,15 @@ void find_bit_pattern(int first_index, int second_index) {
   uint8_t zero_check_arr[3];
   uint8_t one_check_arr[3]; 
 
+  zero_check_arr[0] = 23;
+    zero_check_arr[1] = 24;
+    zero_check_arr[2] = 22;
+
+    one_check_arr[0] = 29;
+    one_check_arr[1] = 30;
+    one_check_arr[2]= 28;
+
+  /*
   if(jetta) {
     zero_check_arr[0] = 16;
     zero_check_arr[1] = 15;
@@ -275,6 +336,7 @@ void find_bit_pattern(int first_index, int second_index) {
     one_check_arr[1] = 6;
     one_check_arr[2] = 6;
   }
+  */
 
   for(int i=first_index;i<second_index;i++) {
       //check for pattern five 0 five 1 five 0 five 1 five 0 five 1 three 0 three 1 three 0 three 1 three 0
@@ -317,7 +379,7 @@ void find_bit_pattern(int first_index, int second_index) {
       }
 
 
-      if(pattern_count==14) {
+      if(pattern_count==10) {
         //break;
 
         ///*
@@ -443,13 +505,19 @@ int main(void)
 
   while(rx_data!=0x14) _spi_read(&hspi1, VERSION, &rx_data, 1);
 
-  if(jetta) _spi_write_multiple(&hspi1, 0x40, JETTA_KEYFOB, sizeof(JETTA_KEYFOB));
-  else _spi_write_multiple(&hspi1, 0x40, HONEYWELL_KEYFOB, sizeof(HONEYWELL_KEYFOB));
+  _spi_write_multiple(&hspi1, 0x40, BMW_KEYFOB, sizeof(BMW_KEYFOB));
+  //if(jetta) _spi_write_multiple(&hspi1, 0x40, JETTA_KEYFOB, sizeof(JETTA_KEYFOB));
+  //else _spi_write_multiple(&hspi1, 0x40, HONEYWELL_KEYFOB, sizeof(HONEYWELL_KEYFOB));
 	//_spi_write_multiple(0x40, cc1100_GFSK_38_4_kb, sizeof(cc1100_GFSK_38_4_kb));
 
 	//315 MHz: 0x0C 0x1D 0x89
   	//434 0x10 0xB0 0x71
 
+    _spi_write(&hspi1, FREQ2,0x10); 
+	  _spi_write(&hspi1, FREQ1,0xB0);
+	  _spi_write(&hspi1, FREQ0,0x71);
+
+  /*
   if(jetta) {
     _spi_write(&hspi1, FREQ2,0x0C); 
 	  _spi_write(&hspi1, FREQ1,0x1D);
@@ -459,6 +527,9 @@ int main(void)
     _spi_write(&hspi1, FREQ1,0x45);
     _spi_write(&hspi1, FREQ0,0x35);
   }
+  */
+
+
 	_spi_write_multiple(&hspi1, PATABLE_BURST, patable_power_315, 8);
 	_spi_write(&hspi1, CHANNR, 0x00);
 	_spi_write(&hspi1, FREND0, 0x11); //adjusts tx power
